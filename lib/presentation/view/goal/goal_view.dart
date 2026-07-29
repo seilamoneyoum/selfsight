77352c3c4e87c435a611ef00a638a496ac22e2d7
seilamoneyoum <seilamoney2002@gmail.com>
2026-07-29 03:56:31 -0400

    Implémentation de tabs de base

--- lib/presentation/view/goal/goal_view.dart
+++ lib/presentation/view/goal/goal_view.dart
@@ -8,47 +8,63 @@ import 'package:stacked/stacked.dart';
 
 class GoalView extends StatefulWidget {
   final String? goalId;
-  const GoalView({
-    Key? key,
-    this.goalId,
-  }) : super(key: key);
+  const GoalView({Key? key, this.goalId}) : super(key: key);
 
   @override
   State<GoalView> createState() => _GoalViewState();
 }
 
 class _GoalViewState extends State<GoalView> {
-  List<VisionBoardItem> list = [];
-
   @override
   Widget build(BuildContext context) {
     final arg = ModalRoute.of(context)?.settings.arguments as GoalViewArguments;
 
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => GoalViewModel(goalId: arg.goalId),
-      onViewModelReady: (viewModel) {
-        viewModel as GoalViewModel;
-        viewModel.loadGoal();
-      },
-      builder: (context, viewModel, child) => Scaffold(
-        appBar: AppBar(
-          title: titleInterface(arg.goalId == null ? "New goal" : "Edit goal"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20.0),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: GoalForm(viewModel: viewModel as GoalViewModel),
-                ),
+      onViewModelReady: (viewModel) =>
+          {viewModel as GoalViewModel, viewModel.loadGoal()},
+      builder: (context, viewModel, child) {
+        return DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title:
+                  titleInterface(arg.goalId == null ? "New goal" : "Edit goal"),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(text: "Goal"),
+                  Tab(text: "Vision board"),
+                  Tab(text: "Tasks"),
+                ],
               ),
-            ],
+            ),
+            body: TabBarView(
+              children: [
+                goalFormContainer(viewModel as GoalViewModel),
+                const Center(child: Icon(Icons.directions_transit, size: 64)),
+                const Center(child: Icon(Icons.directions_bike, size: 64)),
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
+}
+
+Container goalFormContainer(GoalViewModel viewModel) {
+  return Container(
+    margin: EdgeInsets.all(16.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20.0),
+        Expanded(
+          child: SingleChildScrollView(
+            child: GoalForm(viewModel: viewModel),
+          ),
+        ),
+      ],
+    ),
+  );
 }
